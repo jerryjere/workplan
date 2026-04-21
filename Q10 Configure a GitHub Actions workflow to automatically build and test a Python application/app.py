569964f60a01f401070a9151a238ac553e56bcbc
jerryjere <149072258+jerryjere@users.noreply.github.com>
2026-04-22 01:34:17 +0530

    Add files via upload

---
+++ Q10 Configure a GitHub Actions workflow to automatically build and test a Python application/app.py
@@ -0,0 +1,7 @@
+def add(a: int, b: int) -> int:
+    """Return sum of two numbers"""
+    return a + b
+
+def greet(name: str) -> str:
+    """Return greeting message"""
+    return f"Hello, {name}!"
